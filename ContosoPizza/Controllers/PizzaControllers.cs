464f5406222ef343c05bb3cd851b0eaaab2910ae
mag16 <marcoguzman16@gmail.com>
2021-05-25 19:40:05 -0700

    POST

--- ContosoPizza/Controllers/PizzaControllers.cs
+++ ContosoPizza/Controllers/PizzaControllers.cs
@@ -32,9 +32,27 @@ namespace ContosoPizza.Controllers
         }
 
         // POST action
+        [HttpPost]
+        public IActionResult Create(Pizza pizza)
+        {                   
+            // This code will save the pizza and return a result
+            PizzaService.Add(pizza);
+            return CreatedAtAction(nameof(Create), new { id = pizza.Id }, pizza);
+        }       
 
         // PUT action
+        [HttpPut("{id}")]
+        public IActionResult Update(int id, Pizza pizza)
+        {
+            // This code will update the pizza and return a result
+        }
 
         // DELETE action
+        [HttpPut("{id}")]
+        public IActionResult Update(int id, Pizza pizza)
+        {
+            // This code will update the pizza and return a result
+        }
+
     }
 }
